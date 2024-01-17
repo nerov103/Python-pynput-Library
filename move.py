8d8def53fe2ff86620bdb2af0ebf1a08bd2215ea
nerov103 <nibir01000@gmail.com>
2024-01-17 20:14:01 +0600

    added another file

---
+++ move.py
@@ -0,0 +1,12 @@
+from pynput.mouse import Controller
+
+
+muse = Controller()
+
+muse.move(dy=100, dx=10)
+
+"""
+AMIR AKON MOUSE POINTER JA KHANA ASA .SEY KHAN TAKA dy= 100px nesa cola asbo mause pointer and dx= 10 holo mouse pointer ta 10px right mana soja cola asboo
+"""
+
+
---
+++ position.py
@@ -0,0 +1,8 @@
+from pynput.mouse import Controller
+
+mouse = Controller()
+create_pristion = mouse.position
+
+print(f"Your Mouse Pointer Create Proction {create_pristion}")
+
+
